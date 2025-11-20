0c32d6944b3e88c3b3691cc1ab1eba18d56dee07
rayenchallouf <rayenchallouf777@gmail.com>
2025-11-20 15:45:21 +0100

    pushing

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
 // Level is automatically assigned based on rank: 1-5=Elite, 6-10=Advanced, 11-15=Intermediate, 16+=Beginner
 let hackers = [
     { name: "Pl4gueZ", firstBlood: 2, machines: 4, writeUps: 4, writeUps_score: 90 },
-    { name: "br0ker", firstBlood: 1, machines: 4, writeUps: 4, writeUps_score: 10 },
+    { name: "br0ker", firstBlood: 1, machines: 4, writeUps: 4, writeUps_score: 80 },
     { name: "Jesser", firstBlood: 1, machines: 2, writeUps: 2, writeUps_score: 40 },
     { name: "Bilil", firstBlood: 0, machines: 1, writeUps: 1, writeUps_score: 10 },
     { name: "try hack jemmali", firstBlood: 0, machines: 1, writeUps: 1, writeUps_score: 8 },
